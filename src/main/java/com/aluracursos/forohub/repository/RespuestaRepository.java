be092b1708b750b41149817dbda997e80383f31e
castaneda <juxnpxblojpcd315@gmail.com>
2026-03-09 22:31:16 -0500

    creacion de repository para respuesta

---
+++ src/main/java/com/aluracursos/forohub/repository/RespuestaRepository.java
@@ -0,0 +1,8 @@
+package com.aluracursos.forohub.repository;
+
+import com.aluracursos.forohub.domain.respuesta.Respuesta;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+}
